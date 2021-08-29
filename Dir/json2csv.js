c6f40ffd6eef080695208d42daa654cb90c52bda
Swaraj Gupta <50869134+s3arajgupta@users.noreply.github.com>
2021-08-29 22:27:14 +0530

    ok

--- Dir/json2csv.js
+++ Dir/json2csv.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
-var AR = require('./uniqueRepos.json');
+var object = require('./uniqueRepos.json');
 
 //var CS = require('./destination.csv')
 
 const JSONToCSV = require("json2csv").parse;
 
-var csv = JSONToCSV(AR);
-fs.writeFileSync("./Tagged.csv", csv);
+var csv = JSONToCSV(object);
+fs.writeFileSync("./uniqueRepos.csv", csv);
